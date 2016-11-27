55ad739b4334cf139745bd6663fc7177a885647e
Cole Willsea <coleww@gmail.com>
2016-11-27 14:02:04 -0800

    initial html template

---
+++ README.md
@@ -0,0 +1,9 @@
+# VOCALOLOID
+------------------
+
+- just TTS the text
+- control the text
+- just vocode the text against a pre-set melody
+- save the dang thing
+- edit the melody
+- control the synth
---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+$('form').serializeArray()
+// =>
+// [
+//   {"name":"foo","value":"1"},
+//   {"name":"bar","value":"xxx"},
+//   {"name":"this","value":"hi"}
+// ]
+
+// play button => calls playLine, passes it all the data
